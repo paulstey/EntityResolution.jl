fda6b3baf3c268e3c5321d1106472ca96254320b
paulstey <paulstey@gmail.com>
2020-08-16 15:55:33 -0400

    adding some threshold testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,12 +16,34 @@ function candidates(s, itr, sdist, min_score)
 end 
 
 
-candidates("3M", parents_arr, Winkler(Jaro()), 0.99)
+candidates("3M", parents_arr, Winkler(Jaro()), 0.99)           # upper bound
+candidates("3M", parents_arr, Winkler(Jaro()), 0.75)           # lower bound
 
-candidates("A O SMITH", parents_arr, Winkler(Jaro()), 0.86)
-candidates("A O SMITH", parents_arr, Jaccard(2), 0.86)
-candidates("A O SMITH", parents_arr, Overlap(2), 0.99)
+candidates("A O SMITH", parents_arr, Winkler(Jaro()), 0.86)    # upper bound 
+candidates("A O SMITH", parents_arr, Winkler(Jaro()), 0.77)    # lower bound 
+
+candidates("A O SMITH", parents_arr, Jaccard(2), 0.99)         # upper bound 
+candidates("A O SMITH", parents_arr, Jaccard(2), 0.31)         # lower bound 
+
+candidates("A O SMITH", parents_arr, Overlap(2), 0.99)         # upper bound
+candidates("A O SMITH", parents_arr, Overlap(2), 0.63)         # lower bound
 
 candidates("EXXON MOBIL", parents_arr, Winkler(Jaro()), 0.98)  # upper bound
 candidates("EXXON MOBIL", parents_arr, Winkler(Jaro()), 0.75)  # lower bound 
 
+
+candidates("SAINT-GOBAIN", parents_arr, Winkler(Jaro()), 0.87)    # upper bound
+candidates("SAINT-GOBAIN", parents_arr, Winkler(Jaro()), 0.77)    # lower bound
+
+candidates("SAINT-GOBAIN", parents_arr, Overlap(2), 0.77)         # upper bound
+candidates("SAINT-GOBAIN", parents_arr, Overlap(2), 0.51)         # lower bound
+
+
+
+
+###
+# NOTE: Oldcastle is owned by CRH Americas, both own other 
+# so-called parent companies in TRI (e.g., Pike Industries).
+###
+candidates("OLDCASTLE", parents_arr, Overlap(2), 0.87)    # upper bound
+candidates("OLDCASTLE", parents_arr, Overlap(2), 0.63)    # lower bound
